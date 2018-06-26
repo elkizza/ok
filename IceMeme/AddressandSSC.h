ba1a098e483d451f7ab30005f93804639857347e
elkizza <39752208+elkizza@users.noreply.github.com>
2018-06-26 19:35:29 +0100

    Update AddressandSSC.h
    Fucking updated for a day

--- IceMeme/AddressandSSC.h
+++ IceMeme/AddressandSSC.h
@@ -1,18 +1,18 @@
 #pragma once
-DWORD getfieldaddress = ASLR(0x743B20);
-DWORD settopaddress = ASLR(0x745cb0);
-DWORD pushstringaddress = ASLR(0x744EE0);
-DWORD pushvalueaddress = ASLR(0x744FC0);
-DWORD calladdress = ASLR(0x743520);
-DWORD setfieldaddress = ASLR(0x7458E0);
-DWORD pushnumberaddress = ASLR(0x744E50);
-DWORD ScriptContextVFTableaddress = ASLR(0x6c1aee);
-DWORD getmetatableaddress = ASLR(0x743DE0);
-DWORD pcalladdress = ASLR(0x744910);
-DWORD rawrjzaddress = ASLR(0x73cc47);
-DWORD pushbooleanaddress = ASLR(0x7449e0);
-DWORD Identityaddress = ASLR(0x173B1D0);
-DWORD pushniladdress = ASLR(0x744de0);
+DWORD getfieldaddress = ASLR(0x74BE20);
+DWORD settopaddress = ASLR(0x74DFD0);
+DWORD pushstringaddress = ASLR(0x74D1F0);
+DWORD pushvalueaddress = ASLR(0x74D2B0);
+DWORD calladdress = ASLR(0x74b820);
+DWORD setfieldaddress = ASLR(0x74DBF0);
+DWORD pushnumberaddress = ASLR(0x74d160);
+DWORD ScriptContextVFTableaddress = ASLR(0x6c26de);
+DWORD getmetatableaddress = ASLR(0x74C0F0);
+DWORD pcalladdress = ASLR(0x74cc10);
+DWORD rawrjzaddress = ASLR(0x744f57);
+DWORD pushbooleanaddress = ASLR(0x74cce0);
+DWORD Identityaddress = ASLR(0x17AA254);
+DWORD pushniladdress = ASLR(0x74d0f0);
 
 int v41;//ScriptContext
 int v49 = 1;
@@ -22,6 +22,6 @@ void ScanScriptContext() {
 	DWORD ScriptContextVFTable = *(DWORD*)(ScriptContextVFTableaddress + 0x2);
 	v41 = Memory::Scan((char*)&ScriptContextVFTable);
 	//luaState
-	luaState =(v41 + 56 * 1 + 164) - *(DWORD *)(v41 + 56 * 1 + 164);
+	luaState =*(DWORD *)(v41 + 56 * 1 + 164) - (v41 + 56 * 1 + 164);
 	*(DWORD*)Identityaddress = 6;
 }
