cd7fb472b69318f694f5855a60c330007ef32a2f
elkizza <39752208+elkizza@users.noreply.github.com>
2018-06-26 19:40:01 +0100

    UPdated niggaz

--- IceMeme/AddressandSSC.h
+++ IceMeme/AddressandSSC.h
@@ -1,18 +1,18 @@
 #pragma once
-DWORD getfieldaddress = ASLR(0x74BE20);
-DWORD settopaddress = ASLR(0x74DFD0);
-DWORD pushstringaddress = ASLR(0x74D1F0);
-DWORD pushvalueaddress = ASLR(0x74D2B0);
-DWORD calladdress = ASLR(0x74b820);
-DWORD setfieldaddress = ASLR(0x74DBF0);
-DWORD pushnumberaddress = ASLR(0x74d160);
-DWORD ScriptContextVFTableaddress = ASLR(0x6c26de);
-DWORD getmetatableaddress = ASLR(0x74C0F0);
-DWORD pcalladdress = ASLR(0x74cc10);
-DWORD rawrjzaddress = ASLR(0x744f57);
-DWORD pushbooleanaddress = ASLR(0x74cce0);
-DWORD Identityaddress = ASLR(0x17AA254);
-DWORD pushniladdress = ASLR(0x74d0f0);
+DWORD getfieldaddress = ASLR(0x74BE20); // updated
+DWORD settopaddress = ASLR(0x74DFD0);  // updated
+DWORD pushstringaddress = ASLR(0x74D1F0);  // updated
+DWORD pushvalueaddress = ASLR(0x74D2B0);  // updated
+DWORD calladdress = ASLR(0x74b820);  // updated
+DWORD setfieldaddress = ASLR(0x74DBF0);  // updated
+DWORD pushnumberaddress = ASLR(0x74d160); // updated
+DWORD ScriptContextVFTableaddress = ASLR(0x6c26de); // updated
+DWORD getmetatableaddress = ASLR(0x74C0F0); // updated
+DWORD pcalladdress = ASLR(0x74cc10); // updated
+DWORD rawrjzaddress = ASLR(0x744f57); // updated
+DWORD pushbooleanaddress = ASLR(0x74cce0); // updated
+DWORD Identityaddress = ASLR(0x17AA254); // updated
+DWORD pushniladdress = ASLR(0x74d0f0); // updated
 
 int v41;//ScriptContext
 int v49 = 1;
